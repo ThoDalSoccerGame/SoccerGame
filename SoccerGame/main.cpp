cf6f4464d8ff56d52a2317b033bea2f7e30b1a83
Thomas Marques <thomas_marques@orange.fr>
2018-11-20 22:45:32 +0100

    Fullscreen qui s'adapte au VideoMode + background menu

--- SoccerGame/main.cpp
+++ SoccerGame/main.cpp
@@ -3,10 +3,21 @@
 
 using namespace sf;
 
+
 int main()
 {
-	Window window;
-	window.create(VideoMode(), "GameSoccer", Style::Fullscreen);
+	#pragma region VARIABLES
+		RenderWindow window;
+		Texture background;
+		Sprite sprite;
+	#pragma endregion VARIABLES	
+
+	// Creer une fenetre avec le même mode de video que celui du Desktop
+	VideoMode desktop = VideoMode::getDesktopMode();
+	float dWidth = desktop.width;
+	float dHeight = desktop.height;
+	float dBPP = desktop.bitsPerPixel;
+	window.create(VideoMode(dWidth, dHeight, dBPP), "GameSoccer", Style::Fullscreen);
 
 	while (window.isOpen())
 	{
@@ -19,6 +30,27 @@ int main()
 				}
 			}
 		}
+
+		window.clear(Color::Black);
+		
+		// On charge la texture background
+		if (!background.loadFromFile("background.jpg"))
+		{
+			// On affiche une erreur dans la console si le BG n'est pas chargé
+			std::cout << "OU EST CE FOUTU BACKGROUND ??" << std::endl;
+		}
+		// On recupere le facteur d'echelle du BG par rapport à la fenetre
+		long double xScale = dWidth / 1920;
+		long double yScale = dHeight / 1080;
+	
+		// On applique la texture sur une sprite...
+		sprite.setTexture(background);
+		// On met à l'echelle la sprite pour qu'elle rentre dans la fenetre
+		sprite.setScale(xScale, yScale);
+		// On dessine la sprite
+		window.draw(sprite);
+
+		window.display();
 	}
 
 	return 0;
