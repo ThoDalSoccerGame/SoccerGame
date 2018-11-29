df57aff32ac890277599f67fc6496ee530910dde
Gwendal BERNARDI <G.BERNARDI@tiama.com>
2018-11-29 17:10:05 +0100

    Réorganisation

--- SoccerGame/solo.cpp
+++ SoccerGame/solo.cpp
@@ -5,30 +5,64 @@
 using namespace sf;
 using namespace std;
 
+#pragma region InfoCorps
+/*
+Body : x = 70, y = 130
+Head : x = 53, y = 53
+Leg Right : x = 53, y = 138
+Leg Left : x = 53, y = 128
+Arm Right : x = 23, y = 108
+Arm Left: x = 23, y = 108
+*/
+#pragma endregion InfoCorps
+
 #pragma region Variables globales
 
+// Variables sys
 RenderWindow window;
 Event event;
+
+// Variables des sprites/draw
 Texture background, armR, armL, legR, legL, head, body;
 Sprite sprite_background, sprite_armR, sprite_armL, sprite_legR, sprite_legL, sprite_head, sprite_body;
 int x_body = 200, y_body = 375, x_head = 207, y_head = 325, x_legR = 200, y_legR = 495, x_legL = 230, y_legL = 495, x_armL = 180 , y_armL = 375, x_armR = 267, y_armR = 375;
 
+// Redimensionnage de la fenêtre en fonction de l'écran de l'ordinateur 
+VideoMode desktop = VideoMode::getDesktopMode();
+float dWidth = desktop.width;
+float dHeight = desktop.height;
+float dBPP = desktop.bitsPerPixel;
+
+// On récupère la longueur du background en fonction de la taille de la fenêtre :
+long double xScale = dWidth / 1920;
+long double yScale = dHeight / 1080;
+
 #pragma endregion Variables globales
 
+#pragma region Fonction
+
+/* Cette fonction renvoie l'angle du personnage en fonction du sol */
+double ReturnAngle()
+{
+	// Coordonnée de la tête : x_head, y_head
+	// Coordonnée du corps : x_body, y_body
+#pragma region Variables
+	double coefficient_directeur, horizontal_personnage;
+#pragma endregion Variables
+
+	coefficient_directeur = (y_head - y_body) / (x_head - x_body);
+	
+	// Le rapport entre le coefficient directeur du personnage et son horizontal est de -1/7 
+	horizontal_personnage = coefficient_directeur * (-1) ;
+	horizontal_personnage /= 7;
+
+	return horizontal_personnage ;
+}
+
+#pragma endregion Fonction
 
 int main()
 {
-
-#pragma region InfoCorps
-	/*
-	Body : x = 70, y = 130
-	Head : x = 53, y = 53
-	Leg Right : x = 53, y = 138
-	Leg Left : x = 53, y = 128
-	Arm Right : x = 23, y = 108
-	Arm Left: x = 23, y = 108
-	*/
-#pragma endregion InfoCorps
 
 #pragma region Variables 
 
@@ -36,11 +70,6 @@ int main()
 
 #pragma region Window
 
-	// Redimensionnage de la fenêtre en focntion de l'écran de l'ordinateur 
-	VideoMode desktop = VideoMode::getDesktopMode();
-	float dWidth = desktop.width;
-	float dHeight = desktop.height;
-	float dBPP = desktop.bitsPerPixel;
 	window.create(VideoMode(dWidth, dHeight, dBPP), "Partie Solo", Style::Fullscreen);
 
 #pragma endregion Window
@@ -78,7 +107,7 @@ int main()
 						
 						window.display();
 					}
-					
+					returnAngle();
 					break;
 				}
 			}
@@ -92,10 +121,6 @@ int main()
 		if (!background.loadFromFile("../images/background.jpg")) {
 			cout << "OU EST CE FOUTU BACKGROUND ???" << endl;
 		}
-		// On récupère la longueur du background en fonction de la taille de la fenêtre :
-		long double xScale = dWidth / 1920;
-		long double yScale = dHeight / 1080;
-
 		// On applique la texture sur une sprite :
 		sprite_background.setTexture(background);
 		// On met le sprite à l'échelle de la fenetre :
@@ -113,6 +138,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_body.setTexture(body);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_body.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_body);
 		// Position de test du corps :
@@ -125,6 +152,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_head.setTexture(head);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_head.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_head);
 		// Position de test de la tête :
@@ -137,6 +166,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_legL.setTexture(legL);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_legL.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_legL);
 		// Position de test de la jambe gauche :
@@ -149,6 +180,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_legR.setTexture(legR);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_legR.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_legR);
 		// Position de test de la jambre droite
@@ -161,6 +194,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_armL.setTexture(armL);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_armL.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_armL);
 		// Position de test du bras gauche :
@@ -173,6 +208,8 @@ int main()
 		}
 		// On applique la texture sur un sprite :
 		sprite_armR.setTexture(armR);
+		// On redimensionne le personnage par rapport à la résolution de l'écran 
+		sprite_armR.setScale(xScale, yScale);
 		// On dessine le sprite :
 		window.draw(sprite_armR);
 		// Position de test du bras droit :
