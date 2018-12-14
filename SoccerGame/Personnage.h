ebe974f5aa9edfaeb36aad06390e018525a8ab0c
Gwendal BERNARDI <G.BERNARDI@tiama.com>
2018-12-14 12:52:57 +0100

    push

--- SoccerGame/Personnage.h
+++ SoccerGame/Personnage.h
@@ -1,1 +1,26 @@
-#pragma once
+// Head de la classe Personnage du jeu soccer game 
+
+#ifndef DEF_Personnage
+#define DEF_Personnage
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Personnage
+{
+	public :
+
+	private :
+
+
+		// Création des sprites du Corps du personnage
+		sf::Sprite body;
+		sf::Sprite head;
+		sf::Sprite armR;
+		sf::Sprite armL;
+		sf::Sprite legR;
+		sf::Sprite legl;
+
+};
+
+#endif // !DEF_Personnage
