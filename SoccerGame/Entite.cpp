27c5caa60ddd025e5103fe0ea08b74fd7ecbc5ce
Gwendal BERNARDI <G.BERNARDI@tiama.com>
2018-12-20 11:04:34 +0100

    Création de la classe Entite

---
+++ SoccerGame/Entite.cpp
@@ -0,0 +1,16 @@
+#include "Entite.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+// Constructeur de la classe Entite 
+Entite::Entite()
+{
+}
+
+// Destructeur de la classe Entite
+Entite::~Entite()
+{
+}
